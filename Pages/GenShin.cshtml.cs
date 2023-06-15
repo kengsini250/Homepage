a671496dcfa26d5eb7a805c827dd9419ae8ef1dd
おっぱいは愛 <kengsini250@gmail.com>
2023-06-16 07:48:31 +0800

    new page

---
+++ Pages/GenShin.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyHomepage.Pages
+{
+    public class GenShinModel : PageModel
+    {
+        private readonly ILogger<GenShinModel> _logger;
+
+        public GenShinModel(ILogger<GenShinModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void OnGet()
+        {
+
+        }
+    }
+
+}
