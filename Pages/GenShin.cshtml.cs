e01f3b1152e86f235a16145a0a808197db6cc01f
kengsini250 <kengsini250@gmail.com>
2023-06-17 17:39:02 +0800

    model test

--- Pages/GenShin.cshtml.cs
+++ Pages/GenShin.cshtml.cs
@@ -1,28 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyHomepage.Models;
 
-namespace MyHomepage.Pages
+namespace MyHomepage.Pages;
+
+public class GenShinModel : PageModel
 {
-    public class GenShinModel : PageModel
+    private readonly ILogger<GenShinModel> _logger;
+
+    public GenShinModel(ILogger<GenShinModel> logger)
     {
-        private readonly ILogger<GenShinModel> _logger;
+        _logger = logger;
+    }
 
-        public GenShinModel(ILogger<GenShinModel> logger)
+    public string text = "TEST";
+
+    public void OnGet()
+    {
+        text = "TESTSETESTETSETESTETSETEST";
+    }
+
+    [BindProperty] public UsersModel? Users { get; set; }
+    public string a;
+    public string b;
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
-            _logger = logger;
+            return Page();
         }
 
-        public string text = "TEST";
-        public void OnGet()
+        if (Users != null)
         {
-	        text = "TESTSETESTETSETESTETSETEST";
+            a = Users.Username;
+            b = Users.Password;
         }
 
-        public UsersModel Users;
-        public void OnPost()
-        {
-            Users.Username = Request.Query["username"];
-            Users.Password = Request.Query["password"];
-        }
+        return Page();
+        // return RedirectToPage("./Index");
     }
 }
